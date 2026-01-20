0dd3c5229b6249e33c0caeeb1446637dc018ff40
fff <70720317+and777dna@users.noreply.github.com>
2026-01-20 11:17:12 +0100

    Сохраняю текущие изменения before switching the branch

--- LIBRARY/Program.cs
+++ LIBRARY/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("vdfv");
+
+/*Консольный проект + json для хранения данных.
+   - Класс Car (Brand, Model, LicensePlate)
+   - [CRUD](https://skyeng.ru/magazine/wiki/it-industriya/chto-takoe-crud/) операции (создать, получить, обновить, удалить книгу из источника данных)
+   - Поиск по автору/названию/году
+   - Сортировка по популярности (кол-во выдач)
+   - Выдача/возврат книг с датами
+*/
